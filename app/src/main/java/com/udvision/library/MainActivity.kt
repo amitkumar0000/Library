e32c6b0fef4dbfa58548943373bee6c9844bd4ce
amit kumar <amit.ap1924@gmail.com>
2020-05-14 19:56:09 +0530

    added barcode file

--- app/src/main/java/com/udvision/library/MainActivity.kt
+++ app/src/main/java/com/udvision/library/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.udvision.library
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udvision.barcode.BarcodeActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this,BarcodeActivity::class.java))
     }
 }
